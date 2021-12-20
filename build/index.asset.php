298c2cc7e0946ff0f2a443daa4ec4cfebe1b4c56
Ali Alaa <hello@alialaa.com>
2021-12-20 10:23:50 +0200

    Packages Update 20 Dec 21

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'c88423a4810bc4944311d8f6a3ff0b5f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f5e6b814e1de75768b5b381e22df3b7f');
